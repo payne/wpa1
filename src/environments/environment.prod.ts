db8ab7d1a4afd4b09f1cf0036937375a7260177b
Matt Payne <Payne@MattPayne.org>
2022-03-30 19:19:12 -0500

    works once authenticated.  Needs refresh for vote number listener.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'webprojects-38489',
+    appId: '1:119800746358:web:5b47b2a0ceed484b11ea57',
+    storageBucket: 'webprojects-38489.appspot.com',
+    apiKey: 'AIzaSyBY0QyEVIG4fQfsJM-ickRS1zcl-qINaMk',
+    authDomain: 'webprojects-38489.firebaseapp.com',
+    messagingSenderId: '119800746358',
+    measurementId: 'G-76P18JLTHQ',
+  },
   production: true
 };
